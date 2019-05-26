a0c24a5f663c9e38f93b12b2191e6823ef28081f
stathe9 <stathe@affinionds.com>
2019-05-26 23:48:17 +0530

    Update Package

--- src/TranslateCuture.ts
+++ src/TranslateCuture.ts
@@ -1,67 +1,67 @@
-export class TranslateCulture {
-  private templateMatcher: RegExp = /{{\s?([^{}\s]*)\s?}}/g;
+// export class TranslateCulture {
+//   private templateMatcher: RegExp = /{{\s?([^{}\s]*)\s?}}/g;
 
-  // tslint:disable-next-line: ban-types
-  public translateCultures(culture: any, key: string, interpolateParams?: Object) {
-    // var a = this.getValue(this.culture, expr);
-    return this.interpolate(this.getValue(culture, key), interpolateParams);
-  }
+//   // tslint:disable-next-line: ban-types
+//   public translateCultures(culture: any, key: string, interpolateParams?: Object) {
+//     // var a = this.getValue(this.culture, expr);
+//     return this.interpolate(this.getValue(culture, key), interpolateParams);
+//   }
 
-  // tslint:disable-next-line: ban-types
-  private interpolate(expr: string | Function, params?: any): string {
-    let result: string;
+//   // tslint:disable-next-line: ban-types
+//   private interpolate(expr: string | Function, params?: any): string {
+//     let result: string;
 
-    if (typeof expr === 'string') {
-      result = this.interpolateString(expr, params);
-    } else if (typeof expr === 'function') {
-      result = this.interpolateFunction(expr, params);
-    } else {
-      // this should not happen, but an unrelated TranslateService test depends on it
-      result = expr as string;
-    }
+//     if (typeof expr === 'string') {
+//       result = this.interpolateString(expr, params);
+//     } else if (typeof expr === 'function') {
+//       result = this.interpolateFunction(expr, params);
+//     } else {
+//       // this should not happen, but an unrelated TranslateService test depends on it
+//       result = expr as string;
+//     }
 
-    return result;
-  }
+//     return result;
+//   }
 
-  private getValue(target: any, key: string): any {
-    const keys = key.split('.');
-    key = '';
-    do {
-      key += keys.shift();
-      if (this.isDefined(target) && this.isDefined(target[key]) && (typeof target[key] === 'object' || !keys.length)) {
-        target = target[key];
-        key = '';
-      } else if (!keys.length) {
-        target = undefined;
-      } else {
-        key += '.';
-      }
-    } while (keys.length);
+//   private getValue(target: any, key: string): any {
+//     const keys = key.split('.');
+//     key = '';
+//     do {
+//       key += keys.shift();
+//       if (this.isDefined(target) && this.isDefined(target[key]) && (typeof target[key] === 'object' || !keys.length)) {
+//         target = target[key];
+//         key = '';
+//       } else if (!keys.length) {
+//         target = undefined;
+//       } else {
+//         key += '.';
+//       }
+//     } while (keys.length);
 
-    return target;
-  }
+//     return target;
+//   }
 
-  // tslint:disable-next-line: ban-types
-  private interpolateFunction(fn: Function, params?: any) {
-    return fn(params);
-  }
+//   // tslint:disable-next-line: ban-types
+//   private interpolateFunction(fn: Function, params?: any) {
+//     return fn(params);
+//   }
 
-  private interpolateString(expr: string, params?: any) {
-    if (!params) {
-      return expr;
-    }
+//   private interpolateString(expr: string, params?: any) {
+//     if (!params) {
+//       return expr;
+//     }
 
-    return expr.replace(this.templateMatcher, (substring: string, b: string) => {
-      const r = this.getValue(params, b);
-      return this.isDefined(r) ? r : substring;
-    });
-  }
+//     return expr.replace(this.templateMatcher, (substring: string, b: string) => {
+//       const r = this.getValue(params, b);
+//       return this.isDefined(r) ? r : substring;
+//     });
+//   }
 
-  private isDefined(value: any): boolean {
-    return typeof value !== 'undefined' && value !== null;
-  }
+//   private isDefined(value: any): boolean {
+//     return typeof value !== 'undefined' && value !== null;
+//   }
 
-  private isObject(item: any): boolean {
-    return item && typeof item === 'object' && !Array.isArray(item);
-  }
-}
+//   private isObject(item: any): boolean {
+//     return item && typeof item === 'object' && !Array.isArray(item);
+//   }
+// }
