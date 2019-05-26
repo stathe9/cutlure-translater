23a1d5e6438d34a9291eddd98308bf1330bde779
stathe9 <stathe@affinionds.com>
2019-05-26 21:39:33 +0530

    Commit

--- src/TranslateCuture.ts
+++ src/TranslateCuture.ts
@@ -58,7 +58,7 @@ export class TranslateCulture {
     return typeof value !== 'undefined' && value !== null;
   }
 
-  isObject(item: any): boolean {
+  private isObject(item: any): boolean {
     return item && typeof item === 'object' && !Array.isArray(item);
   }
 }
